d7e6a0fd19f6828d879c914b287cc06afd69a745
ErasylAbenov <abenov.erasyl@gmail.com>
2020-05-21 22:42:57 +0600

    Аннотация  @PostConstruct, @PreDestroy.

--- src/main/Java/kz/era/springcourse/ClassicalMusic.java
+++ src/main/Java/kz/era/springcourse/ClassicalMusic.java
@@ -3,9 +3,22 @@ package kz.era.springcourse;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing My Initialization!");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my Destruction");
+    }
 
     @Override
     public String getSong() {
