6a7722564be2430583b54fcdbcda30fb2e2da064
ErasylAbenov <abenov.erasyl@gmail.com>
2020-05-23 10:17:27 +0600

    Аннотация @ComponentScan, without Configuration xml

---
+++ src/main/Java/kz/era/springcourse/SpringConfig.java
@@ -0,0 +1,11 @@
+package kz.era.springcourse;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("kz.era.springcourse")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+}
--- src/main/Java/kz/era/springcourse/TestSpring.java
+++ src/main/Java/kz/era/springcourse/TestSpring.java
@@ -1,23 +1,13 @@
 package kz.era.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-
-//        Music classicalMusic = context.getBean("classicalMusic", Music.class);
-//        MusicPlayer classicalMusicPlayer = new MusicPlayer(classicalMusic);
-//        classicalMusicPlayer.playMusic();
-//
-//        Music rockMusic = context.getBean("rockMusic", Music.class);
-//        MusicPlayer rockMusicPlayer = new MusicPlayer(rockMusic);
-//        rockMusicPlayer.playMusic();
-
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
